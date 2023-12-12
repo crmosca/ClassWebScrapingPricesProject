d430291227d2f7fb5e4cca1bf92ce4b1110d982a
Rocco Mosca <christianrmosca@gmail.com>
2023-12-11 22:24:20 -0800

    Add data analysis script

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -20,11 +20,44 @@ average_price_per_brand = all_data.groupby('Brand')['Price'].mean()
 #calculate average price for each listing 
 average_price_per_listing = all_data.groupby('Listing Name')['Price'].mean()
 
+#now onto doing the data for the individual websites
+#calculating the average price for each brand and listing on ebay
+average_price_per_brand_ebay = ebay_data.groupby('Brand')['Price'].mean()
+average_price_per_listing_ebay = ebay_data.groupby('Listing Name')['Price'].mean()
+
+#calculate average price for each brand name and listing on amazon
+average_price_per_brand_amazon = amazon_data.groupby('Brand')['Price'].mean()
+average_price_per_listing_amazon = amazon_data.groupby('Listing Name')['Price'].mean()
+
+#calculate average price for each brand name and listing on google shopping
+average_price_per_brand_google = google_shopping_data.groupby('Brand')['Price'].mean()
+average_price_per_listing_google = google_shopping_data.groupby('Listing Name')['Price'].mean()
+
 
 #display results
 print(f"Analysis for search term: {search_term}")
-print("Average Price for each brand:")
+
+print("\nOverall Analysis:")
+print("Average Price for each brand (All Data):")
 print(average_price_per_brand)
 
-print("\nAverage Price for Each Listing:")
+print("\nAverage Price for Each Listing (All Data):")
 print(average_price_per_listing)
+
+print("\nAnalysis for Ebay:")
+print("\nAverage Price for Each Brand (Ebay):")
+print(average_price_per_listing_ebay)
+
+print("\nAnalysis for Amazon:")
+print("Average Price for Each Brand (Amazon):")
+print(average_price_per_brand_amazon)
+
+print("\nAverage Price for Each Listing (Amazon):")
+print(average_price_per_listing_amazon)
+
+print("\nAnalysis for Google Shopping:")
+print("\nAverage Price for Each Brand (Google Shopping):")
+print(average_price_per_brand_google)
+
+print("\nAverage Price per Each Listing (Google Shopping)")
+print(average_price_per_listing_google)
