94956a454b543673a12185943f5648c384f2f3c3
Rocco Mosca <christianrmosca@gmail.com>
2023-12-12 21:27:52 -0800

    Initial commmit

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -2,34 +2,34 @@ import pandas as pd
 import re
 
 def clean_filename(filename):
-    # Remove invalid characters from the filename
+    #removing invalid characters from the filename
     return re.sub(r'[<>:"/\\|?*]', '', filename)
 
-# Get user input for the search term
-search_term = input("Enter the product name that you want to search for on eBay: ")
+#getting user input for the search term
+search_term = input("Enter the product name that you searched for on eBay (make sure you write it exactly the same way): ")
 
-# Loading the data from the CSV file
+#loading the data from the CSV file
 cleaned_search_term = clean_filename(search_term)
 csv_file_path = f'data/raw_data/{cleaned_search_term}_ebay_prices_and_names.csv'
 ebay_data = pd.read_csv(csv_file_path)
 
-# Data cleaning: convert 'Prices' column to numeric, get rid of non-numeric symbols, handle ranges
+#data cleaning: convert 'Prices' column to numeric, get rid of non-numeric symbols, handle ranges
 ebay_data['Prices'] = ebay_data['Prices'].replace('[\$,]', '', regex=True)
-# Handling cases like '2.00to79.00' by taking the average of the range
+#handling cases like '2.00to79.00' by taking the average of the range
 ebay_data['Prices'] = ebay_data['Prices'].apply(lambda x: 
                                                 (float(x.split('to')[0]) + float(x.split('to')[1])) / 2 
                                                 if isinstance(x, str) and 'to' in x else float(x))
 
-# Clean the listing names by removing special characters
+#cleaning the listing names by removing special characters
 ebay_data['Listing Names'] = ebay_data['Listing Names'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
 
-# Save the cleaned data back to the CSV file
+#saving the cleaned data back to the CSV file
 ebay_data.to_csv(csv_file_path, index=False)
 
-# Calculating average price for each listing on eBay
+#calculating average price for each listing on eBay
 average_price_per_listing_ebay = ebay_data.groupby('Listing Names')['Prices'].mean()
 
-# Display results
+#displaying results
 print(f"Analysis for search term: {search_term}")
 
 print("\nAnalysis for eBay:")
--- visualizations/visualizing_data.py
+++ visualizations/visualizing_data.py
@@ -5,33 +5,33 @@ import matplotlib.pyplot as plt
 import re
 
 def clean_filename(filename):
-    # Remove invalid characters from the filename
+    #removing invalid characters from the filename
     return re.sub(r'[<>:"/\\|?*]', '', filename)
 
-# Get user input for the search term
-search_term = input("Enter the product name that you want to visualize on eBay: ")
+#getting user input for the search term
+search_term = input("Enter the product name that you want to visualize from eBay (make sure you write it exactly the same way): ")
 
-# Clean the search term for filename
+#cleaning the search term for filename
 cleaned_search_term = clean_filename(search_term)
 
-# Construct the absolute file path
+#constructing the absolute file path
 script_directory = os.path.dirname(os.path.abspath(__file__))
 csv_file_path = os.path.join(script_directory, f'../data/raw_data/{cleaned_search_term}_ebay_prices_and_names.csv')
 
-# Load the data from the CSV file
+#loading the data from the CSV file
 ebay_data = pd.read_csv(csv_file_path)
 
-# Convert 'Prices' column to numeric
+#converting 'Prices' column to numeric
 ebay_data['Prices'] = ebay_data['Prices'].replace('[\$,]', '', regex=True).astype(float)
 
-# Creating a boxplot for eBay data
+#creating a boxplot for eBay data
 plt.figure(figsize=(10, 6))
 sns.boxplot(x='Listing Names', y='Prices', data=ebay_data)
 plt.title('Boxplot of Prices for eBay Data')
 plt.xticks(rotation=90)
 plt.show()
 
-# Creating a violin plot for Prices with Listing Names
+#creating a violin plot for Prices with Listing Names
 plt.figure(figsize=(12, 6))
 sns.violinplot(x='Listing Names', y='Prices', data=ebay_data, inner='quartile', cut=0)
 plt.title('Violin Plot of Prices for eBay Data')
@@ -40,14 +40,7 @@ plt.xlabel('Listing Names')
 plt.ylabel('Prices')
 plt.show()
 
-# Creating a FacetGrid of KDE plots for each Listing Name
-g = sns.FacetGrid(ebay_data, col='Listing Names', col_wrap=4, height=4, sharex=False, sharey=False)
-g.map(sns.kdeplot, 'Prices', fill=True)
-g.set_axis_labels('Prices', 'Density')
-g.set_titles(col_template="{col_name}")
-plt.show()
-
-# Print basic statistical data
+#printing basic statistical data
 statistical_data = ebay_data['Prices'].describe().to_frame().transpose()
 print("\nStatistical Data for Prices:")
 print(statistical_data)
