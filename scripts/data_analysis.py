818128e2176da7264dc5305997847cff4c9c9221
Rocco Mosca <christianrmosca@gmail.com>
2023-12-12 20:22:44 -0800

    fixed data analysis to clean the file name due to spaces and to remove extraneous symbols from numbers

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,18 +1,27 @@
 import pandas as pd
+import re
 
-#loading the data from the CSV file
-ebay_data = pd.read_csv('data/raw_data/<your_search_term>_ebay_prices_and_names.csv')
+def clean_filename(filename):
+    # Remove invalid characters from the filename
+    return re.sub(r'[<>:"/\\|?*]', '', filename)
 
-#extracting the search term from the metadata (assuming it's in the first row)
-search_term = ebay_data.iloc[0]['Search Term']
+# Get user input for the search term
+search_term = input("Enter the product name that you want to search for on eBay: ")
 
-#data cleaning: convert 'Prices' column to numeric, get rid of non-numeric symbols, convert to float
-ebay_data['Prices'] = ebay_data['Prices'].replace('[\$,]', '', regex=True).astype(float)
+# Loading the data from the CSV file
+cleaned_search_term = clean_filename(search_term)
+csv_file_path = f'data/raw_data/{cleaned_search_term}_ebay_prices_and_names.csv'
+ebay_data = pd.read_csv(csv_file_path)
 
-#calculating average price for each listing on eBay
+# Data cleaning: convert 'Prices' column to numeric, get rid of non-numeric symbols, handle ranges
+ebay_data['Prices'] = ebay_data['Prices'].replace('[\$,]', '', regex=True)
+# Handling cases like '2.00to79.00' by taking the average of the range
+ebay_data['Prices'] = ebay_data['Prices'].apply(lambda x: (float(x.split('to')[0]) + float(x.split('to')[1])) / 2 if 'to' in x else float(x))
+
+# Calculating average price for each listing on eBay
 average_price_per_listing_ebay = ebay_data.groupby('Listing Names')['Prices'].mean()
 
-#display results
+# Display results
 print(f"Analysis for search term: {search_term}")
 
 print("\nAnalysis for eBay:")
